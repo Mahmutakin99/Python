2fba528ea04666b794ec378982221224bd31d87d
Mahmutakin99 <147618707+Mahmutakin99@users.noreply.github.com>
2025-01-27 01:07:22 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,72 @@
+question_data = [
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Linus Torvalds, Linux ve Git'i oluşturdu.",
+        "dogru_cevap": "True",
+        "yanlis_cevaplar": ["False"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Windows 7 işletim sisteminin altı ana sürümü vardır.",
+        "dogru_cevap": "True",
+        "yanlis_cevaplar": ["Yanlış"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Orijinal C programlama dilinde işaretçiler (pointers) kullanılmadı; bunlar daha sonra C++'a eklendi.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Nvidia GTX 1080 adını yalnızca 1920x1080 ekran çözünürlüğünde render yapabilmesinden alıyor.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Linux ilk olarak Windows XP'ye alternatif olarak oluşturuldu.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "RAM, Rastgele Erişimli Bellek anlamına gelir.",
+        "dogru_cevap": "True",
+        "yanlis_cevaplar": ["Yanlış"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Bilgisayarlardaki zaman, EPOX sistemi ile ölçülür.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Çoğu programlama dilinde '++' operatörü, '+= 1' ifadesine eşdeğerdir.",
+        "dogru_cevap": "True",
+        "yanlis_cevaplar": ["Yanlış"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Python programlama dili, JavaScript'in değiştirilmiş bir versiyonuna dayanır.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    },
+    {
+        "kategori": "Bilim: Bilgisayarlar",
+        "zorluk": "Kolay",
+        "soru": "Snapchat'in logosu bir çandır.",
+        "dogru_cevap": "False",
+        "yanlis_cevaplar": ["Doğru"]
+    }
+]
